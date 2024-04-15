2971578c9318ea7a82020286d89f53ac0af8a736
2Kelvin <rocketmancodes02@gmail.com>
2024-04-15 17:38:41 +0300

    style the application

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
         // App's starting point
         home: MyHomePage(),
@@ -54,20 +54,53 @@ class MyHomePage extends StatelessWidget {
     // watch track's for changes int the App's state (MyAppState)
     // the state changes when a new word is generated i.e. when current variable changes
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Generating an awesome random name:'),
-          Text(appState.current.asCamelCase),
-          // adding a generate next name elevated button
-          ElevatedButton(
-              onPressed: () {
-                appState.getNextWordPair();
-                print('New Word-pair generated!');
-              },
-              child: Text('Next Name'))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            // adding a generate next name elevated button
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNextWordPair();
+                  print('New word-pair generated!');
+                },
+                child: Text('Generate Name'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
